f6d9df877408cb45854a84f2458631f3146f0741
czl <2847247006@qq.com>
2022-03-05 13:15:37 +0800

    hotfix commit2

--- demo/src/main/java/com/czl/demo.java
+++ demo/src/main/java/com/czl/demo.java
@@ -4,6 +4,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("asd");
         System.out.println("hot-fix");
+        System.out.println("hot-fix2");
 
     }
 }
